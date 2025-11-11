78024eb66f84321a8fba5dcec7db47e0ac346450
diorgius <diorgius@gmail.com>
2025-11-11 17:13:25 +0300

    added task 3

--- app.php
+++ app.php
@@ -13,14 +13,40 @@ function getFullnameFromParts($surname, $name, $patronomyc) {
     return $snp = $surname.' '.$name.' '.$patronomyc;
 }
 
-// print_r(getPartsFromFullname($snp));
-// print_r(getFullnameFromParts($surname, $name, $patronomyc));
+print_r(getPartsFromFullname($snp));
+echo '<br>';
+echo getFullnameFromParts($surname, $name, $patronomyc).'<br>';
 
 function getShortName($snp){
     $surname = mb_substr(getPartsFromFullname($snp)['surname'], 0, 1).'.';
     $name = getPartsFromFullname($snp)['name'];
-    $shortName = $name.' '.$surname;
-    return $shortName;
+    $short_name = $name.' '.$surname;
+    return $short_name;
 }
 
-print_r(getShortName($snp));
+echo getShortName($snp).'<br>';
+
+function getGenderFromName($snp) {
+    $gender_мark = 0;
+    $surname = getPartsFromFullname($snp)['surname'];
+    $name = getPartsFromFullname($snp)['name'];
+    $patronomyc = getPartsFromFullname($snp)['patronomyc'];
+    if(mb_substr($surname, -2, 2) === 'ва') {
+        $gender_мark -= 1;
+    } elseif (mb_substr($surname, -1, 1) === 'в') {
+        $gender_мark += 1;
+    }
+    if(mb_substr($name, -1, 1) === 'а') {
+        $gender_мark -= 1;
+    } elseif(mb_substr($name, -1, 1) === 'й' || mb_substr($name, -1, 1) === 'н') {
+        $gender_мark += 1;
+    }
+    if(mb_substr($patronomyc, -3, 3) === 'вна') {
+        $gender_мark -= 1;
+    } elseif (mb_substr($patronomyc, -2, 2) === 'ич'){
+        $gender_мark += 1;
+    }
+    return $gender_мark <=> 0;
+}
+
+echo getGenderFromName($snp).'<br>';
