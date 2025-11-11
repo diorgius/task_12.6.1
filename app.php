30e6836c659c99fb8cd95c2e70d324cb98b30da3
diorgius <diorgius@gmail.com>
2025-11-11 14:53:42 +0300

    added task 1

--- app.php
+++ app.php
@@ -1,1 +1,17 @@
 <?php
+$snp = 'Иванов Иван Иванович';
+$surname = 'Иванов';
+$name = 'Иван';
+$patronomyc = 'Иванович';
+
+function getPartsFromFullname($snp) {
+    $key_array = ['surname', 'name', 'patronomyc']; 
+    return $snp_array = array_combine( $key_array, explode(' ', $snp));
+}
+
+function getFullnameFromParts($surname, $name, $patronomyc) {
+    return $snp = $surname.' '.$name.' '.$patronomyc;
+}
+
+print_r(getPartsFromFullname($snp));
+print_r(getFullnameFromParts($surname, $name, $patronomyc));
